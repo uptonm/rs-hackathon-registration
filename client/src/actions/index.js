787c8a9aebeda8f50ba57414be9e1977883dfb35
Michael Upton <uptonm@wit.edu>
2019-04-01 08:24:37 -0400

    fix

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -8,14 +8,14 @@ export const fetchUser = () => async dispatch => {
 
 export const fetchUsers = async () => {
   const res = await axios.get(
-    `/api/users`
+    `/api/users/?key=${process.env.REACT_APP_QUERYKEY}`
   );
   return res.data;
 };
 
 export const putUser = async (id, data) => {
   const res = await axios.put(
-    `/api/users/${id}`,
+    `/api/users/${id}/?key=${process.env.REACT_APP_QUERYKEY}`,
     data
   );
   return res.data;
