3b0a4fb69463d78e064efaf6c4a3a78b0164e66e
Mike Upton <uptonm@wit.edu>
2019-04-12 18:32:23 -0400

    build

--- client/src/components/CodeOfConduct.js
+++ client/src/components/CodeOfConduct.js
@@ -6,17 +6,19 @@ export default () => {
       <h1 className="display-4">Code of Conduct</h1>
       <p>April 12th, 2019</p>
       <p>
-        HackWITus values inclusion and accessibility for all of our community
-        members. We strive to foster a strong and compassionate community that
-        values individual respect above all else. Nobody in our community should
-        ever have to feel harassed or uncomfortable whether online or at an
-        event. It is everyone’s responsibility to create this healthy
-        environment.
+        Rocket.Build Community Hackathon values inclusion and accessibility for
+        all of our community members. We strive to foster a strong and
+        compassionate community that values individual respect above all else.
+        Nobody in our community should ever have to feel harassed or
+        uncomfortable whether online or at an event. It is everyone’s
+        responsibility to create this healthy environment.
       </p>
       <br />
 
       <h1 className="display-4">Anti-harassment</h1>
-      <h6 className="display-4" style={{fontSize: '1.75rem'}}>Harassment includes, but is not limited to:</h6>
+      <h6 className="display-4" style={{ fontSize: '1.75rem' }}>
+        Harassment includes, but is not limited to:
+      </h6>
       <ul>
         <li>
           Offensive verbal or written comments related to gender, age, sexual
@@ -38,20 +40,33 @@ export default () => {
       <p>
         If you feel uncomfortable or think there may be a potential violation of
         the code of conduct, please report it immediately using one of the
-        following methods. All reporters have the right to remain anonymous.
+        following methods.{' '}
+        <strong>All reporters have the right to remain anonymous.</strong>
       </p>
-    <br/>
-      <p>Corbin Hakimian - Head Student Organizer - <a href="mailto:hakimianc@wit.edu">hakimianc@wit.edu</a></p>
-      <br/>
+      <br />
+      <ul>
+        <li>
+          Sara Dell - Rocket.Build Community Organizer -{' '}
+          <a href="mailto:dells@wit.edu">dells@wit.edu</a>
+        </li>
+        <li>
+          Faith Bade - Rocket.Build Community Organizer -{' '}
+          <a href="mailto:badef@wit.edu">badef@wit.edu</a>
+        </li>
+      </ul>
+      <br />
 
       <p>
         If you do not feel comfortable reporting to either of the previously
         mentioned contacts you can fill out the following form online:
+        <a href="https://rshackathon.typeform.com/to/hMtlGT">
+          Online Incident Report Form
+        </a>
       </p>
       <h1 className="display-4">Considerations</h1>
       <p>
         This Code of Conduct was inspired by MLH’s CoC and written in
-        coordination with Wentworth’s Diversity Center. 
+        coordination with Wentworth’s Diversity Center. If you have any questions about this document email <a href="mailto:dells@wit.edu">dells@wit.edu</a>
       </p>
     </div>
   );
